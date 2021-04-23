5e0c4106450e27cee027001de33f8058a579f9f0
mauseba <maurosebastianb1@gmail.com>
2021-04-23 00:54:45 -0500

    Menu final

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -8,7 +8,7 @@
          <a class="btn btn-success float-right" href="{{route('admin.categories.create')}}">Agregar categoría</a>
     @endcan
 
-    <h1>Lista de categorías</h1>
+    <h1>Lista de Comisiones</h1>
 @stop
 
 @section('content')
